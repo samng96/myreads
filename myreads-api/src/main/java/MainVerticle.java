902f24dfa3f7a4e981a579977c49492ea9ee5072
Sam Ng <samng96@gmail.com>
2018-01-26 00:04:40 -0800

    setting up initial http server

---
+++ myreads-api/src/main/java/MainVerticle.java
@@ -0,0 +1,26 @@
+import io.vertx.core.AbstractVerticle;
+import io.vertx.core.DeploymentOptions;
+import io.vertx.core.Vertx;
+import io.vertx.core.json.JsonObject;
+
+public class MainVerticle extends AbstractVerticle {
+
+    @Override
+    public void start() throws Exception {
+        vertx.createHttpServer().requestHandler(req -> {
+              req.response()
+                .putHeader("content-type", "text/plain")
+                .end("myReads API service");
+            }).listen(8080);
+        System.out.println("HTTP server started on port 8080");
+    }
+
+    public static void main(String[] args) {
+        Vertx vertx = Vertx.vertx();
+        DeploymentOptions deploymentOptions = new DeploymentOptions().setConfig(new JsonObject()
+                .put("http.port", 8081)
+                .put("myreads_host", "localhost")
+        );
+        vertx.deployVerticle(MainVerticle.class.getName(), deploymentOptions);
+    }
+}
