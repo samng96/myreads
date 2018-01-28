cdc7ba0461d8af1e18ee3cba552ac0546240842c
Sam Ng <samng96@gmail.com>
2018-01-27 21:11:12 -0800

    adding test class and adding router to start method

--- myreads-api/src/main/java/MainVerticle.java
+++ myreads-api/src/main/java/MainVerticle.java
@@ -1,17 +1,30 @@
 import io.vertx.core.AbstractVerticle;
 import io.vertx.core.DeploymentOptions;
+import io.vertx.core.Future;
 import io.vertx.core.Vertx;
+import io.vertx.core.http.HttpServerResponse;
 import io.vertx.core.json.JsonObject;
+import io.vertx.ext.web.Router;
 
 public class MainVerticle extends AbstractVerticle {
 
     @Override
-    public void start() throws Exception {
-        vertx.createHttpServer().requestHandler(req -> {
-              req.response()
-                .putHeader("content-type", "text/plain")
-                .end("myReads API service");
-            }).listen(8080);
+    public void start(Future<Void> fut) throws Exception {
+        Router router = Router.router(vertx);
+
+        router.route("/").handler(routingContext -> {
+                    HttpServerResponse response = routingContext.response();
+                    response
+                            .putHeader("content-type", "text/plain")
+                            .end("myReads API service");
+                });
+
+        vertx.createHttpServer().requestHandler(router::accept).listen(8080, result -> {
+            if (result.succeeded()) {
+                fut.complete();
+            } else {
+                fut.fail(result.cause());
+            }});
         System.out.println("HTTP server started on port 8080");
     }
 
